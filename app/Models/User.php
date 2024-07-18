10b865a78083cfbdc7cace3add0225b7d2ec1360
Abdellah <abdellah.qc@gmail.com>
2024-07-18 13:26:13 -0400

    add sanctum

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,11 @@ use App\Models\Contracts\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable implements Model
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
